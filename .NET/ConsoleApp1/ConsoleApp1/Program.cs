d5dfcfd2d954c582db6d921383b0b2519736d9f4
Nez <sanyapoom.siri@gmail.com>
2022-11-17 22:43:51 -0800

    Add project files.

---
+++ .NET/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+public class App {
+    private static void WriteSomething() {
+        Console.WriteLine("Something Something!");
+    }
+    private static void WriteSomethingSpecific(string input) {
+        Console.WriteLine(input);
+    }
+    public static void Main(string[] args) {
+        WriteSomething();
+        WriteSomethingSpecific("AHHHHHHHHH");
+        //asdad
+        
+        float thing = 3.2f;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(thing);
+    }
+}
---
+++ practice-NET6-WinForm/Form1.cs
@@ -0,0 +1,7 @@
+namespace practice_NET6_WinForm {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
